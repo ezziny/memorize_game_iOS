33e78fd9126fe22ea8d57a3d4b054b53cce4c57c
ezziny <ezzinynone@gmail.com>
2024-08-01 16:15:16 +0300

    Initial Commit

---
+++ assignment 1/assignment_1App.swift
@@ -0,0 +1,17 @@
+//
+//  assignment_1App.swift
+//  assignment 1
+//
+//  Created by ezz on 01/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct assignment_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
